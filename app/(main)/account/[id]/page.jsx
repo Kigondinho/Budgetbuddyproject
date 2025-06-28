9f582b3134c02ca80e97035035483010e28fbea6
Albert Thuku <athuku02@gmail.com>
2025-06-28 04:26:50 +0300

    Seeding transaction data into database linked to account id

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,8 +1,34 @@
+import { getAccountWithTransactioins } from '@/actions/accounts'
+import { notFound } from 'next/navigation';
 import React from 'react'
 
-const AccountsPage = ({params}) => {
+const AccountsPage = async ({params}) => {
+
+  const {id} = await params;
+  const accountData = await getAccountWithTransactioins(id);
+
+  if(!accountData){
+    notFound()
+  }
+
+  const {transactions, ...account} = accountData;
+
   return (
-    <div>{params.id}</div>
+    <div className="space-y-8 px-5 flex gap-4 items-end justify-between">
+      <div>
+        <h1 className="text-5xl sm:text-6xl font-bold gradient-title capitalize">{account.name}</h1>
+        <p className="text-muted-foreground">{account.type.charAt(0) + account.type.slice(1).toLowerCase()} Account</p>
+      </div>
+      <div className="text-right pb-2">
+        <div className="text-xl sm:text-2xl font-bold">${parseFloat(account.balance).toFixed(2)}</div>
+        <p className="text-sm text-muted-foreground">{account._count.transactions} Transactions </p>
+      </div>
+
+      {/* Chart section */}
+
+      {/* Transaction table */}
+      
+    </div>
   )
 }
 
