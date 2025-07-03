ca8eb244289f9006d7320f894f23462f102a4e26
Albert Thuku <athuku02@gmail.com>
2025-07-03 08:52:03 +0300

    AI financial report insights in monthly report email

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlerts, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/function";
+import { checkBudgetAlerts, generateMonthlyReports, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    checkBudgetAlerts, processRecurringTransaction ,triggerRecurringTransactions
+    checkBudgetAlerts, processRecurringTransaction ,triggerRecurringTransactions, generateMonthlyReports
   ],
 });
